7c7684e4b7a10ebdfb5b42185f5215b11e3bf626
Robert Khasanov <robert.khasanov@tu-dresden.de>
2026-09-08 20:16:06 +0200

    update nauty download URL and add SHA256 hash for verification

--- cmake/GetNauty.cmake
+++ cmake/GetNauty.cmake
@@ -8,7 +8,8 @@ set(EXTRA_CFLAGS "'-O3 -fPIC'")
 set(NCPU 4)
 
 ExternalProject_Add(nauty_traces
-  URL               http://pallini.di.uniroma1.it/nauty26r10.tar.gz
+  URL               https://users.cecs.anu.edu.au/~bdm/nauty/nauty26r10.tar.gz
+  URL_HASH          SHA256=065f8d08f2c3f301f47b472a26ec8e2d7d8750d671722248afe61b03209ded9a
   SOURCE_DIR        "${NAUTY_WORK_DIR}"
   BINARY_DIR        "${NAUTY_WORK_DIR}"
   CONFIGURE_COMMAND "${NAUTY_WORK_DIR}/configure" "CFLAGS=${EXTRA_CFLAGS}"
